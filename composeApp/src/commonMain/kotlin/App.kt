146a3982021059fbd00883906dea7f14fc1f503d
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2024-04-28 15:17:07 +0200

    Added background image

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,3 +1,4 @@
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
@@ -9,9 +10,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -19,73 +23,108 @@ fun App() {
     MaterialTheme {
         val cardElements: MutableList<CardElement> = mutableListOf()
 
-        // Main button row
-        Row(
+        // Background Image
+        Image(
+            painterResource(DrawableResource("background.jpg")),
+            contentDescription = "backgroundImage",
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 16.dp, horizontal = 16.dp)
-        ) {
-            Button(
-                onClick = {
-                    cardElements.add(TextElement())
-                },
-                modifier = Modifier.padding(horizontal = 8.dp)
-            ) {
-                Text("Add Text")
-            }
-            Button(
-                onClick = {
-                    cardElements.add(ImageElement())
-                },
-                modifier = Modifier.padding(horizontal = 8.dp)
-            ) {
-                Text("Add Image")
-            }
-        }
+                .aspectRatio(ratio = 3456f / 2304f)
+        )
 
-        Row(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            // Config column
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .weight(0.65f)
-                    .padding(vertical =16.dp, horizontal = 16.dp)
-            ) {
-
-            }
-
-            // Card Preview Column
-            Column(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .weight(0.3f)
-                    .padding(vertical =16.dp, horizontal = 16.dp)
-            ) {
-                // Card preview
-                Row(
-                    modifier = Modifier
-                        .weight(0.9f)
-                        .align(Alignment.CenterHorizontally)
-                ) {
-                    CardPreview(
-                        modifier = Modifier
+        // Main UI
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(48.dp)
+                .clip(
+                    RoundedCornerShape(
+                        topStartPercent = 2,
+                        topEndPercent = 2,
+                        bottomStartPercent = 2,
+                        bottomEndPercent = 2
                     )
-                }
-                // Download buttons
-                Row(
-                    modifier = Modifier
-                        .weight(0.1f)
-                        .align(Alignment.CenterHorizontally)
+                )
+                .background(Color.White.copy(alpha = 0.9f))
+                .border(
+                    width = 1.dp,
+                    color = Color.Black,
+                    shape = RoundedCornerShape(
+                        topStartPercent = 2,
+                        topEndPercent = 2,
+                        bottomStartPercent = 2,
+                        bottomEndPercent = 2
+                    )
+                )
+        ) {
+            // Main button row
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp, horizontal = 16.dp)
+            ) {
+                Button(
+                    onClick = {
+                        cardElements.add(TextElement())
+                    },
+                    modifier = Modifier.padding(horizontal = 16.dp)
                 ) {
-                    Button(
-                        onClick = {
-                            // Render image and download png
-                        },
-                        modifier = Modifier.padding(horizontal = 8.dp)
+                    Text("Add Text")
+                }
+                Button(
+                    onClick = {
+                        cardElements.add(ImageElement())
+                    },
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                ) {
+                    Text("Add Image")
+                }
+            }
+
+            Row(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                // Config column
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(0.65f)
+                        .padding(vertical = 16.dp, horizontal = 16.dp)
+                ) {
+
+                }
+
+                // Card Preview Column
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(0.3f)
+                        .padding(vertical = 16.dp, horizontal = 16.dp)
+                ) {
+                    // Card preview
+                    Row(
+                        modifier = Modifier
+                            .weight(0.9f)
+                            .align(Alignment.CenterHorizontally)
                     ) {
-                        Text("Export as PNG")
+                        CardPreview(
+                            modifier = Modifier
+                        )
+                    }
+                    // Download buttons
+                    Row(
+                        modifier = Modifier
+                            .weight(0.1f)
+                            .align(Alignment.CenterHorizontally)
+                    ) {
+                        Button(
+                            onClick = {
+                                // Render image and download png
+                            },
+                            modifier = Modifier.padding(horizontal = 8.dp)
+                        ) {
+                            Text("Export as PNG")
+                        }
                     }
                 }
             }
@@ -98,9 +137,16 @@ fun CardPreview(modifier: Modifier = Modifier) {
     Box(
         modifier = modifier
             .size(width = 750.dp, height = 1050.dp)
-            // This is 2.5 inches by 3.5 inches, at 300 dpi
-            //.aspectRatio(ratio = 2.5f / 3.5f)
-            .padding(vertical =16.dp, horizontal = 16.dp)
+            .aspectRatio(2.5f / 3.5f)
+            .padding(vertical = 16.dp, horizontal = 16.dp)
+            .clip(
+                RoundedCornerShape(
+                    topStartPercent = 10,
+                    topEndPercent = 10,
+                    bottomStartPercent = 10,
+                    bottomEndPercent = 10
+                )
+            )
             .background(Color.White)
             .border(
                 width = 1.dp,
