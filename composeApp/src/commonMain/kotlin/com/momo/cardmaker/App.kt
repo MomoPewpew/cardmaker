991bffeed990e9b344f42bad540a9d134d6f34ed
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2024-04-30 16:53:29 +0200

    Default button color to green
    Also, pin button changes color when in use

--- composeApp/src/commonMain/kotlin/com/momo/cardmaker/App.kt
+++ composeApp/src/commonMain/kotlin/com/momo/cardmaker/App.kt
@@ -3,10 +3,7 @@ package com.momo.cardmaker
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.PushPin
 import androidx.compose.runtime.*
@@ -35,213 +32,218 @@ object PinningState {
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
-    MaterialTheme {
-
-    }
-
-    Box(
-        modifier = Modifier
-    ) {
-        // Background Image
-        Image(
-            painterResource(DrawableResource("background.jpg")),
-            contentDescription = "backgroundImage",
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop
+    MaterialTheme(
+        colors = lightColors(
+            primary = Color(0xFF013220)
         )
-
-        // Main UI
-        Row(
+    ) {
+        Box(
             modifier = Modifier
-                .padding(48.dp)
         ) {
-            // Config column
-            Column(
+            // Background Image
+            Image(
+                painterResource(DrawableResource("background.jpg")),
+                contentDescription = "backgroundImage",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+
+            // Main UI
+            Row(
                 modifier = Modifier
-                    .weight(0.65f)
-                    .fillMaxHeight()
-                    .clip(
-                        RoundedCornerShape(
-                            topStartPercent = 2,
-                            topEndPercent = 2,
-                            bottomStartPercent = 2,
-                            bottomEndPercent = 2
-                        )
-                    )
-                    .background(Color.White.copy(alpha = 0.9f))
-                    .border(
-                        width = 1.dp,
-                        color = Color.Black,
-                        shape = RoundedCornerShape(
-                            topStartPercent = 2,
-                            topEndPercent = 2,
-                            bottomStartPercent = 2,
-                            bottomEndPercent = 2
-                        )
-                    )
+                    .padding(48.dp)
             ) {
-                var advancedFolded by remember { mutableStateOf(false) }
-
-                // Main button row
-                Row(
+                // Config column
+                Column(
                     modifier = Modifier
-                        .padding(vertical = 8.dp, horizontal = 32.dp)
-                        .height(48.dp)
-                ) {
-                    Button(
-                        onClick = {
-                            card.cardElements.add(TextElement())
-                            advancedFolded =
-                                true // Fold before unfolding to ensure a recomposition of the advanced segment
-                            advancedFolded = false
-                        },
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .padding(horizontal = 16.dp)
-                    ) {
-                        Text("Add Text")
-                    }
-                    Button(
-                        onClick = {
-                            card.cardElements.add(ImageElement())
-                            advancedFolded =
-                                true // Fold before unfolding to ensure a recomposition of the  advanced segment
-                            advancedFolded = false
-                        },
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .padding(horizontal = 16.dp)
-                    ) {
-                        Text("Add Image")
-                    }
-                    Button(
-                        onClick = {
-                            PinningState.togglePinning()
-                        },
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .padding(horizontal = 16.dp)
-                    ) {
-                        Icon(imageVector = Icons.Outlined.PushPin, contentDescription = "Decrease")
-                    }
-                }
-
-                // Line
-                Row(
-                    modifier = Modifier
-                        .padding(vertical = 0.dp, horizontal = 16.dp)
-                ) {
-                    Canvas(modifier = Modifier.fillMaxWidth()) {
-                        drawLine(
-                            start = Offset(x = 0f, y = 0f),
-                            end = Offset(x = size.width, y = 0f),
-                            color = Color.Black.copy(alpha = 0.8f),
-                            strokeWidth = 1f
+                        .weight(0.65f)
+                        .fillMaxHeight()
+                        .clip(
+                            RoundedCornerShape(
+                                topStartPercent = 2,
+                                topEndPercent = 2,
+                                bottomStartPercent = 2,
+                                bottomEndPercent = 2
+                            )
                         )
-                    }
-                }
-
-                // Pinned
-                var pinnedFolded by remember { mutableStateOf(false) }
-                Row(modifier = Modifier) {
-                    Text(
-                        text = if (pinnedFolded) "▲ Pinned" else "▼ Pinned",
-                        modifier = Modifier
-                            .padding(top = 16.dp, start = 32.dp)
-                            .clickable { pinnedFolded = !pinnedFolded },
-                        style = MaterialTheme.typography.h3
-                    )
-                }
-                Row(
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .defaultMinSize(minHeight = 5.dp)
-                        .fillMaxWidth()
+                        .background(Color.White.copy(alpha = 0.9f))
                         .border(
                             width = 1.dp,
-                            Color.Black.copy(alpha = 0.2f),
+                            color = Color.Black,
                             shape = RoundedCornerShape(
-                                topStart = 15.dp,
-                                topEnd = 15.dp,
-                                bottomStart = 15.dp,
-                                bottomEnd = 15.dp
+                                topStartPercent = 2,
+                                topEndPercent = 2,
+                                bottomStartPercent = 2,
+                                bottomEndPercent = 2
                             )
                         )
                 ) {
-                    if (!pinnedFolded && (PinningState.state.value || !PinningState.state.value)) { // This seemingly redundant check is made to force a recomposition after a new pin is made
-                        Column(modifier = Modifier.fillMaxWidth()) {
-                            for (cardElement in card.cardElements) {
-                                cardElement.buildPinnedElements(modifier = Modifier)
-                            }
+                    var advancedFolded by remember { mutableStateOf(false) }
+
+                    // Main button row
+                    Row(
+                        modifier = Modifier
+                            .padding(vertical = 8.dp, horizontal = 32.dp)
+                            .height(48.dp)
+                    ) {
+                        Button(
+                            onClick = {
+                                card.cardElements.add(TextElement())
+                                advancedFolded =
+                                    true // Fold before unfolding to ensure a recomposition of the advanced segment
+                                advancedFolded = false
+                            },
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .padding(horizontal = 16.dp)
+                        ) {
+                            Text("Add Text")
+                        }
+                        Button(
+                            onClick = {
+                                card.cardElements.add(ImageElement())
+                                advancedFolded =
+                                    true // Fold before unfolding to ensure a recomposition of the  advanced segment
+                                advancedFolded = false
+                            },
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .padding(horizontal = 16.dp)
+                        ) {
+                            Text("Add Image")
+                        }
+
+                        val buttonColor = if (!PinningState.state.value) Color(0xFF013220) else Color.Gray
+                        Button(
+                            onClick = {
+                                PinningState.togglePinning()
+                            },
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .padding(horizontal = 16.dp),
+                            colors = ButtonDefaults.buttonColors(backgroundColor = buttonColor)
+                        ) {
+                            Icon(imageVector = Icons.Outlined.PushPin, contentDescription = "Pin")
                         }
                     }
-                }
 
-                // Advanced
-                Row(modifier = Modifier) {
-                    Text(
-                        text = if (advancedFolded) "▲ Advanced" else "▼ Advanced",
+                    // Line
+                    Row(
                         modifier = Modifier
-                            .padding(top = 16.dp, start = 32.dp)
-                            .clickable { advancedFolded = !advancedFolded },
-                        style = MaterialTheme.typography.h3
-                    )
-                }
-                Row(
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .defaultMinSize(minHeight = 5.dp)
-                        .fillMaxWidth()
-                        .border(
-                            width = 1.dp,
-                            Color.Black.copy(alpha = 0.2f),
-                            shape = RoundedCornerShape(
-                                topStart = 15.dp,
-                                topEnd = 15.dp,
-                                bottomStart = 15.dp,
-                                bottomEnd = 15.dp
+                            .padding(vertical = 0.dp, horizontal = 16.dp)
+                    ) {
+                        Canvas(modifier = Modifier.fillMaxWidth()) {
+                            drawLine(
+                                start = Offset(x = 0f, y = 0f),
+                                end = Offset(x = size.width, y = 0f),
+                                color = Color.Black.copy(alpha = 0.8f),
+                                strokeWidth = 1f
                             )
+                        }
+                    }
+
+                    // Pinned
+                    var pinnedFolded by remember { mutableStateOf(false) }
+                    Row(modifier = Modifier) {
+                        Text(
+                            text = if (pinnedFolded) "▲ Pinned" else "▼ Pinned",
+                            modifier = Modifier
+                                .padding(top = 16.dp, start = 32.dp)
+                                .clickable { pinnedFolded = !pinnedFolded },
+                            style = MaterialTheme.typography.h3
                         )
-                ) {
-                    if (!advancedFolded) {
-                        Column(modifier = Modifier.fillMaxWidth()) {
-                            for (cardElement in card.cardElements) {
-                                cardElement.buildElements(
-                                    modifier = Modifier
+                    }
+                    Row(
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .defaultMinSize(minHeight = 5.dp)
+                            .fillMaxWidth()
+                            .border(
+                                width = 1.dp,
+                                Color.Black.copy(alpha = 0.2f),
+                                shape = RoundedCornerShape(
+                                    topStart = 15.dp,
+                                    topEnd = 15.dp,
+                                    bottomStart = 15.dp,
+                                    bottomEnd = 15.dp
                                 )
+                            )
+                    ) {
+                        if (!pinnedFolded && (PinningState.state.value || !PinningState.state.value)) { // This seemingly redundant check is made to force a recomposition after a new pin is made
+                            Column(modifier = Modifier.fillMaxWidth()) {
+                                for (cardElement in card.cardElements) {
+                                    cardElement.buildPinnedElements(modifier = Modifier)
+                                }
+                            }
+                        }
+                    }
+
+                    // Advanced
+                    Row(modifier = Modifier) {
+                        Text(
+                            text = if (advancedFolded) "▲ Advanced" else "▼ Advanced",
+                            modifier = Modifier
+                                .padding(top = 16.dp, start = 32.dp)
+                                .clickable { advancedFolded = !advancedFolded },
+                            style = MaterialTheme.typography.h3
+                        )
+                    }
+                    Row(
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .defaultMinSize(minHeight = 5.dp)
+                            .fillMaxWidth()
+                            .border(
+                                width = 1.dp,
+                                Color.Black.copy(alpha = 0.2f),
+                                shape = RoundedCornerShape(
+                                    topStart = 15.dp,
+                                    topEnd = 15.dp,
+                                    bottomStart = 15.dp,
+                                    bottomEnd = 15.dp
+                                )
+                            )
+                    ) {
+                        if (!advancedFolded) {
+                            Column(modifier = Modifier.fillMaxWidth()) {
+                                for (cardElement in card.cardElements) {
+                                    cardElement.buildElements(
+                                        modifier = Modifier
+                                    )
+                                }
                             }
                         }
                     }
                 }
-            }
 
-            // Card Preview Column
-            Column(
-                modifier = Modifier
-                    .weight(0.35f)
-                    .padding(start = 16.dp)
-            ) {
-                // Card preview
-                Row(
+                // Card Preview Column
+                Column(
                     modifier = Modifier
-                        .align(Alignment.CenterHorizontally)
-                        .weight(weight = 0.9f, fill = false)
+                        .weight(0.35f)
+                        .padding(start = 16.dp)
                 ) {
-                    CardPreview()
-                }
-                // Download buttons
-                Row(
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally)
-                        .weight(0.1f)
-                ) {
-                    Button(
-                        onClick = {
-                            // Render image and download png
-                        },
-                        modifier = Modifier.padding(horizontal = 16.dp)
+                    // Card preview
+                    Row(
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .weight(weight = 0.9f, fill = false)
                     ) {
-                        Text("Export as PNG")
+                        CardPreview()
+                    }
+                    // Download buttons
+                    Row(
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .weight(0.1f)
+                    ) {
+                        Button(
+                            onClick = {
+                                // Render image and download png
+                            },
+                            modifier = Modifier.padding(horizontal = 16.dp)
+                        ) {
+                            Text("Export as PNG")
+                        }
                     }
                 }
             }
