f77526669175854e7ae902366567faa3d0586d2f
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2024-05-08 19:59:04 +0200

    Added save as dialogue to desktopMain

--- composeApp/src/desktopMain/kotlin/ImageUtilsDesktop.kt
+++ composeApp/src/desktopMain/kotlin/ImageUtilsDesktop.kt
@@ -1,11 +1,12 @@
-import androidx.compose.ui.graphics.asSkiaBitmap
 import com.momo.cardmaker.ImageUtils
 import org.jetbrains.skia.Bitmap
 import org.jetbrains.skiko.toBufferedImage
+import java.awt.Frame
 import java.awt.image.BufferedImage
 import java.io.File
-import java.io.FileOutputStream
 import javax.imageio.ImageIO
+import javax.swing.JFileChooser
+import javax.swing.filechooser.FileNameExtensionFilter
 
 expect object ImageUtilsDesktop : ImageUtils {
     override suspend fun saveBitmapToFile(bitmap: Bitmap, filePath: String)
@@ -18,9 +19,39 @@ actual object ImageUtilsDesktop : ImageUtils {
         graphics.drawImage(bitmap.toBufferedImage(), 0, 0, null)
         graphics.dispose()
 
-        val file = File(filePath)
-        val outputStream = FileOutputStream(file)
-        ImageIO.write(bufferedImage, "png", outputStream)
-        outputStream.close()
+        // Get the default download directory
+        val defaultDir = System.getProperty("user.home")
+
+        // Create a JFileChooser instance
+        val fileChooser = JFileChooser(defaultDir)
+        fileChooser.dialogTitle = "Save Image"
+
+        // Create a FileFilter for PNG files
+        val pngFilter = FileNameExtensionFilter("PNG Image", "png")
+        fileChooser.setFileFilter(pngFilter)
+
+        // Show the save dialog
+        val returnVal = fileChooser.showSaveDialog(Frame())
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            val selectedFile = fileChooser.selectedFile
+
+            // Write the image to the selected file
+            ImageIO.write(bufferedImage, "png", selectedFile)
+
+            // Ensure filename ends with .png
+            val filename = selectedFile.absolutePath
+            val extension = if (filename.lastIndexOf('.') > 0) {
+                filename.substring(filename.lastIndexOf('.'))
+            } else {
+                ""
+            }
+
+            if (extension.lowercase() != ".png") {
+                val newFilename = "$filename.png"
+                selectedFile.renameTo(File(newFilename))
+            }
+
+        }
     }
 }
