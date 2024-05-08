bec671b48600e4f17dace5ea0bb1b44711e1b151
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2024-05-08 22:54:08 +0200

    New approach to wasmJs image exporting

--- composeApp/src/wasmJsMain/kotlin/ImageUtilsWasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/ImageUtilsWasmJs.kt
@@ -1,28 +1,47 @@
 import com.momo.cardmaker.ImageUtils
-import kotlinx.browser.document
 import org.jetbrains.skia.Bitmap
+import kotlinx.browser.window
+import org.khronos.webgl.Uint8ClampedArray
+import org.khronos.webgl.set
+import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.HTMLAnchorElement
-import org.w3c.dom.url.URL
-import org.w3c.files.Blob
-import org.w3c.files.BlobPropertyBag
+import org.w3c.dom.HTMLCanvasElement
 
 object ImageUtilsWasmJs : ImageUtils {
     override suspend fun saveBitmapToFile(bitmap: Bitmap, filePath: String) {
-        val byteArray = bitmap.readPixels()
+        // Create a canvas element
+        val canvas = window.document.createElement("canvas") as HTMLCanvasElement
+        val context = canvas.getContext("2d") as CanvasRenderingContext2D
 
-        if (byteArray != null) {
-            val jsArray = JsArray<JsAny?>()
+        // Set canvas dimensions to match bitmap
+        canvas.width = bitmap.width
+        canvas.height = bitmap.height
 
-            val blob = Blob(blobParts = jsArray, options = BlobPropertyBag(type = "image/png"))
-            val url = URL.createObjectURL(blob)
+        val bitArray = bitmap.readPixels()
+        if (bitArray != null) {
+            // Create Skia Image from Bitmap
+            val clampedArray: Uint8ClampedArray = Uint8ClampedArray(bitArray.size)
 
-            val link = document.createElement("card") as HTMLAnchorElement
-            link.href = url
-            link.download = "card.png"
+            for (i in 1..<bitArray.size) {
+                clampedArray.set(i, bitArray.get(i))
+            }
 
-            link.click()
+            val imageData = context.createImageData(bitmap.width.toDouble(), bitmap.height.toDouble())
+            imageData.data.set(clampedArray)
 
-            URL.revokeObjectURL(url)
+            // Draw Skia Image onto canvas
+            context.putImageData(imageData, 0.0, 0.0)
+
+            // Convert canvas content to PNG data URL
+            val dataUrl = canvas.toDataURL("image/png")
+
+            // Create a link element and set its properties
+            val downloadLink = window.document.createElement("a") as HTMLAnchorElement
+            downloadLink.href = dataUrl
+            downloadLink.download = filePath
+
+            // Simulate a click on the link to trigger the download
+            downloadLink.click()
         }
     }
 }
